2d992d8b7c4e3a9f488a59d803024f3647478a91
choa293 <choa293@newschool.edu>
2021-04-13 13:23:17 -0700

    progress

---
+++ first.js
@@ -0,0 +1,39 @@
+// function myFunction() {
+//   var element = document.getElementById("definition");
+//   element.classList.toggle("header1");
+// }
+
+function toggleText() {
+  var text = document.getElementById("definition");
+  if (text.style.display === "none") {
+    text.style.display = "block";
+  } else {
+    text.style.display = "none";
+  }
+}
+
+function toggleText2() {
+  var text = document.getElementById("wikipedia");
+  if (text.style.display === "none") {
+    text.style.display = "block";
+  } else {
+    text.style.display = "none";
+  }
+}
+
+function toggleText3() {
+  var text = document.getElementById("psychology");
+  if (text.style.display === "none") {
+    text.style.display = "block";
+  } else {
+    text.style.display = "none";
+  }
+}
+
+// function mouseDown() {
+//   document.getElementByClass("header1").style.color = "red";
+// }
+
+// function mouseUp() {
+//   document.getElementByClass("header1").style.color = "green";
+// }
